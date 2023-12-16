a4febd908acf66770fd271361a32e91c4c8971dc
sunghyun <tjdgus9773@gmail.com>
2023-12-16 16:33:02 +0900

    1215 react+spring class

---
+++ sts4/SpringReactProject/src/main/java/boot/data/controller/ShopController.java
@@ -0,0 +1,13 @@
+package boot.data.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+//react와 연결하기 때문에 @RestController 사
+@RestController
+//서로 다른 도메인을 연결해주는 어노테이션
+@CrossOrigin
+public class ShopController {
+
+	
+}
